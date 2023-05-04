df6616429e4b5dffa2aa41733f4ce0b2ab2d2470
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-05-04 17:16:40 -0500

    trying to connect to api

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 //this one is to get the lat and lon of a city: 
-var apiCity = 'https://api.openweathermap.org/geo/1.0/direct?q={cityname}&limit=5&appid=d91f911bcf2c0f925fb6535547a5ddc9'
+
 
 //this one is for the lat and lon to get the weather: 
 var apiLatLon = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=d91f911bcf2c0f925fb6535547a5ddc9'
@@ -11,12 +11,14 @@ var apiIcons = 'https://api.openweathermap.org/img/w/{icon}.png'
 //d91f911bcf2c0f925fb6535547a5ddc9
 
 function getCity () {
-    fetch(apiCity).then(function(response) {
-        return response.json();
-    })
-    .then(function (data) { 
+    var apiCity = 'https://api.openweathermap.org/geo/1.0/direct?q={cityname}&limit=5&appid=d91f911bcf2c0f925fb6535547a5ddc9';   
+    fetch(apiCity).then(function (response) {
+        console.log(response.status);
+       return response.json()
+       .then(function (data) { 
         console.log(data);
+        
     })
-
-    
+})
 }
+getCity();
