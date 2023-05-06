ba598bcf59bca07bc9ad58ff9cb4bb670f560587
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-05-06 18:52:39 -0500

    completed getCity function

--- assets/script.js
+++ assets/script.js
@@ -15,33 +15,33 @@ var searchHandler = function (event) {
 if (cityName) {
     getCity(cityName)
     console.log(cityName)
+} else if (cityName.length === 0) {
+    document.querySelector('#notvalidcity').textContent = "sorry can't find that city"
 }
 }
 
 function getCity (cityName) {
     var apiCity = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + ',US&limit=5&appid=a4b52d54f93021519848eaf25cda8f87';   
     fetch(apiCity).then(function (response) {
-        console.log(response.status);
        return response.json()
        .then(function (data)  {
-        var lat = data[0].lat 
-        var lon = data[0].lon
-        if (cityName.length === 0){
-            document.querySelector('#notvalidcity').textContent = "sorry can't find that city"
-        }
-        console.log(data);        
+        const lat = data[0].lat 
+        const lon = data[0].lon
+        console.log(data);     
+        getLatLon(lat, lon);   
     })
     
 
 })
 };
 var getLatLon = function (lat, lon) {
- var cityLatLon = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&' + lon '=&appid=a4b52d54f93021519848eaf25cda8f87&units=imperial';
-fetch(cityLatLon).then(function(response) )
-
-}
-
-
-
+ var cityLatLon = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=a4b52d54f93021519848eaf25cda8f87&units=imperial`;
+fetch(cityLatLon).then(function (response) {
+console.log(response.status);
+    return response.json().then(function (data) {
+    console.log(data);
+});
+});
+};
 
 searchButton.addEventListener('click', searchHandler);
